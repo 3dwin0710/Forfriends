4b8459edfd48764ed69d57b8722d4d60547588e1
Li Edwin <eli@.et.esiea.fr>
2022-02-19 22:46:25 +0100

    for a js

--- form.js
+++ form.js
@@ -1,1 +1,55 @@
-alert('Test');
+document.write("Hello World");
+
+let pointdevie = 10;
+
+if (pointdevie > 0)
+{
+  pointdevie -= 1;
+}
+else {
+  alert("Game over");
+}
+
+
+function multiply(num1,num2) {
+  let result = num1 * num2;
+  return result;
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+
+function setUserName() {
+  let myName = prompt('Veuillez saisir votre nom.');
+  localStorage.setItem('nom', myName);
+  myHeading.textContent = 'Mozilla est cool, ' + myName;
+}
+
+if (!localStorage.getItem('nom')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('nom');
+  myHeading.textContent = 'Mozilla est cool, ' + storedName;
+}
+
+myButton.addEventListener('click', function() {
+  setUserName();
+});
+
+
+
+/*document.querySelector('html').addEventListener('click', function() {
+
+
+    document.write(pointdevie);
+
+
+});
+
+*/
+
+
+
+document.write(" ");
+document.write(pointdevie);
